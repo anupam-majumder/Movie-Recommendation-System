b67c65b03980a6bc54f8be295baac8e8e3a00ab9
Sumit <sumit.bhuin@students.iiit.ac.in>
2020-04-23 21:11:01 +0530

    added database

--- src/app/html/static/js/api.js
+++ src/app/html/static/js/api.js
@@ -3,7 +3,7 @@ app.controller('apiController',function($scope,$http){
 $scope.callLogin=function(userid){
 	user = userid;
 	$http.post('http://localhost:12394/login',userid).then(function readData(response){
-	console.log(response.data.userid);
+		console.log(response.data.userid);
         console.log(response.data.recommends);
         $scope.mv=response.data.recommends;
 	}
