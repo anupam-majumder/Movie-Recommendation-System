0873b8a5294c42cdbf734104e3d95835582d1b65
Sumit <sumit.bhuin@students.iiit.ac.in>
2020-04-23 20:59:23 +0530

    added database connectivity

--- src/app/api.py
+++ src/app/api.py
@@ -3,11 +3,21 @@ import mysql.connector
 
 app = Flask(__name__)
 
+__port = 12394
+__host = '0.0.0.0'
+__database = 'recomendation'
+__localhost = 'localhost'
+__user = 'root'
+__password = '12345678'
+__table = 'Movies'
+
+
+connection = mysql.connector.connect(host=__localhost,database=__database,user=__user,password=__password)
+
 @app.route('/')
 def home():
-    #return render_template('index.html')
-    #need to train the model
-    print("server is on")
+    print("Movie recommendsation system is on!")
+
 
 @app.route('/welcome', methods=['GET', 'POST'])
 def welcome():
@@ -16,17 +26,34 @@ def welcome():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    print("hi this is here")
     print(request.data)
     if request.method == 'POST':
-        print("hi this is inside post")
-        return json.dumps({"recommends":[{"url":"https://images-na.ssl-images-amazon.com/images/M/MV5BNDE0NTQ1NjQzM15BMl5BanBnXkFtZTYwNDI4MDU5..jpg","movie":"movie1"},{"url":"https://images-na.ssl-images-amazon.com/images/M/MV5BNDE0NTQ1NjQzM15BMl5BanBnXkFtZTYwNDI4MDU5..jpg","movie":"movie2"},{"url":"https://images-na.ssl-images-amazon.com/images/M/MV5BNDE0NTQ1NjQzM15BMl5BanBnXkFtZTYwNDI4MDU5..jpg","movie":"movie3"}],"userid":request.data})
+        #method call for prediction
+
+        #get movie url from db
+        movie_lists = {}
+        movie_lists["recommends"] = __get_movies(["mad love","terminator","hulk"])
+        movie_lists["userid"] =  request.data
+
+        return json.dumps(movie_lists)
+
     else:
         return json.dumps({"success":True})
     
-def __get_movies():
-    pass
+def __get_movies(predicted):
+    lst_of_movies = []
+    if connection.is_connected():
+        for movie in predicted:
+            sql_select_Query = "select movie_url from "+__table+" where movie_title="+"'"+movie+"';"
+            cursor = connection.cursor()
+            cursor.execute(sql_select_Query)
+            records = cursor.fetchall()
+            movie_obj = {}
+            movie_obj["movie"] = movie
+            movie_obj["url"] = records[0]
+            lst_of_movies.append(movie_obj)
+    return lst_of_movies
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=12394, debug=True)
+    app.run(host=__host, port=__port, debug=True)
 
