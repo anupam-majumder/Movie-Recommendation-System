d9706e9f983ddeb6c299b6c9dc466d3885029f81
vm-iiit <virat.mishra@students.iiit.ac.in>
2020-04-25 23:21:29 +0530

    Minor modifications to integrated_model

--- src/app/recommender/integrated_model.py
+++ src/app/recommender/integrated_model.py
@@ -26,7 +26,6 @@ def mapping(train) :
     data = []
 
     for urid,irid,r,timestamp in train :
-
         try:
             uid = uid_dict[urid]
         except KeyError:
@@ -53,16 +52,9 @@ def parse_line(line):
     return uid, iid, float(r), timestamp
 
 def Read_Data(file_name,shuffle=True) :
-
-    # data = pd.read_csv(file_name,sep = "\t", names = ["uid","iid","r","timst"])
-    # data.sort_values(by = "timst", inplace = True)
-
-    # train = data.head(n = int(0.8 * data.shape[0]))
-    # test = data.drop(train.index)
     with open(os.path.expanduser(file_name)) as f:
         raw_ratings = [parse_line(line) for line in itertools.islice(f, 0, None)]
     if shuffle:
-        # train_test shuffle in reproducable manner
         np.random.seed(400)
         np.random.shuffle(raw_ratings)
 
@@ -71,7 +63,6 @@ def Read_Data(file_name,shuffle=True) :
     train_sparse,uid,iid = mapping(raw_ratings[:math.ceil(raw_len*0.8)])
     test = raw_ratings[math.ceil(raw_len*0.8):]
     return train_sparse,uid,iid,test
-
 
 def all_ratings(matrix,axis=1):
 
@@ -105,8 +96,6 @@ def get_user_means(matrix):
         users_mean[u] = np.mean(get_user(matrix,u)[1])
     return users_mean
 
-
-
 def train(train_sparse,test, n_epochs = 30, n_factors = 20) :
 
     matrix = train_sparse.tocsc()
@@ -126,7 +115,6 @@ def train(train_sparse,test, n_epochs = 30, n_factors = 20) :
     lr = 0.007
     reg = 0.001
     n_reg = 0.015
-
     reg7 = 0.005
 
     for current_epoch in range(n_epochs):
@@ -138,7 +126,6 @@ def train(train_sparse,test, n_epochs = 30, n_factors = 20) :
             Nu = get_user(matrix,u)[0]
             I_Nu = len(Nu)
             sqrt_N_u = np.sqrt(I_Nu)
-
             
             y_u = np.sum(item_pref_factor[Nu], axis=0)
 
@@ -148,16 +135,11 @@ def train(train_sparse,test, n_epochs = 30, n_factors = 20) :
 
             w_ij = np.dot((get_user(matrix,u)[1] - global_mean - user_bias[u] - item_bias[Nu]) ,neighbouhood_weights[i][Nu])
 
-
             c_w =  (c_ij + w_ij )/sqrt_N_u
-
            
             rp = global_mean + user_bias[u] + item_bias[i] + np.dot(item_factor[i], user_factor[u] + u_impl_prf) + c_w
 
-            
             e_ui = r - rp
-
-            #sgd
 
             user_bias[u] += lr * (e_ui - reg7 * user_bias[u])
             item_bias[i] += lr * (e_ui - reg7 * item_bias[i])
@@ -170,15 +152,13 @@ def train(train_sparse,test, n_epochs = 30, n_factors = 20) :
             for j in Nu :
                 implicit_feedback[i,j] += n_lr * ((e_ui / sqrt_N_u) - n_reg * implicit_feedback[i,j])
 
-
-
-
         n_lr *= 0.9
         lr *= 0.9
         print("Time For Epoch :: "+str(datetime.now()-start))
      
     return user_bias,item_bias,item_pref_factor,implicit_feedback,neighbouhood_weights,item_factor,user_factor,global_mean
 
-train_dataset, uid_dict, iid_dict, test_dataset = Read_Data(file_name,True)
-user_bias,item_bias,item_pref_factor,implicit_feedback,neighbouhood_weights,item_factor,user_factor,global_mean = train(train_dataset,test_dataset,30)
-np.savez("../integrated_model",user_bias,item_bias,item_pref_factor,implicit_feedback,neighbouhood_weights,item_factor,user_factor,global_mean)
+if __name__ == "__main__":
+	train_dataset, uid_dict, iid_dict, test_dataset = Read_Data(file_name,True)
+	user_bias,item_bias,item_pref_factor,implicit_feedback,neighbouhood_weights,item_factor,user_factor,global_mean = train(train_dataset,test_dataset,30)
+	np.savez("integrated_model",user_bias,item_bias,item_pref_factor,implicit_feedback,neighbouhood_weights,item_factor,user_factor,global_mean)	
