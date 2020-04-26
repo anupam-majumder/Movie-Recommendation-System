f518f6ec67cbb2cd8581fa56421e8857c4d0eb20
vm-iiit <virat.mishra@students.iiit.ac.in>
2020-04-26 21:13:41 +0530

    dump new ratings to tempfile

---
+++ src/app/recommender/rating_updater.py
@@ -0,0 +1,17 @@
+def txt_dump(list_of_tuples):
+
+	delimiter = '|'
+	with open('temp.txt', mode='w') as file_handler:
+		for _tuple in list_of_tuples:
+			user = str(_tuple[0])
+			movie = str(_tuple[1])
+			rating = str(_tuple[2])
+			timestamp = str(_tuple[3])
+
+			file_handler.write(user+delimiter+movie+delimiter+rating+delimiter+timestamp+'\n')
+		file_handler.close()
+
+if __name__=="__main__":
+	sample_list = [('a', 'b', 'c', 'd'), (1, 2, 3, 4), ('a', 1, 'b', '2')]
+	txt_dump(sample_list)
+
